eb9a8592f8045ad7fe901e99b711782877aeb333
kazumasa8 <kazumasa.inagaki@gmail.com>
2025-03-30 16:09:17 +0900

    アクティブストレージ導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :scheduled_id, :price, :image).merge(user_id: current_user.id)
   end
 end
