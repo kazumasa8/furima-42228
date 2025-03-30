34811c2f5a92975f28751b998f396076d66205e7
kazumasa8 <kazumasa.inagaki@gmail.com>
2025-03-30 19:06:21 +0900

    モデルの単体テストコードを書く

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :scheduled_id, numericality: { other_than: 1 }
-end
-
 end
