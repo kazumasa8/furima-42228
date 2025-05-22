6cdda07eecd3f536d39459c87c38bfe2c9212d2a
kazumasa8 <kazumasa.inagaki@gmail.com>
2025-05-22 14:13:09 +0900

    Revert "修正"
    This reverts commit d577c4066aec3b91e581c96045434ef21aefa4f8.

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
   has_one :order
 
   validates :name, presence: true
-  validates :image, presence: true
   validates :description, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
